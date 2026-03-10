63dc7b9f01a87aadd64526158e043450b6628596
Hillary Ramirez <HillaryAdrid-Ramirez@outlook.com>
2026-03-10 14:10:15 +0100

    corregir error de vite

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,8 @@
 
         <!-- Scripts -->
         @routes
-        @viteReact(['resources/css/app.css', 'resources/js/app.jsx'])
+        @viteReactRefresh
+        @vite(['resources/js/app.jsx', "resources/js/Pages/{$page['component']}.jsx"])
         @inertiaHead
     </head>
     <body class="font-sans antialiased">
