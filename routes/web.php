d2dac024ccb6ffa621690f8faf1cedb5ac481594
Hillary Ramirez <HillaryAdrid-Ramirez@outlook.com>
2026-05-20 12:25:40 +0200

    maximo 5 partidas en el historial

--- routes/web.php
+++ routes/web.php
@@ -35,7 +35,7 @@ Route::get('/dashboard', function () {
         )
         ->groupBy('juegos.juego_id', 'juegos.updated_at', 'juegos.temperatura')
         ->orderBy('juegos.updated_at', 'desc')
-        ->limit(10)
+        ->limit(5)
         ->get();
 
     $formattedHistory = $history->map(function ($row) {
